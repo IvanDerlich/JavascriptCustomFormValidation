d2ddb5093e54131b031b53a128bd10adc28618fd
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-04-28 05:37:29 -0300

    Improve password validation

--- scripts/password.js
+++ scripts/password.js
@@ -1,1 +1,11 @@
-console.log("password file")
+const password = document.querySelector('#password')
+console.log(password)
+
+const passwordRepeat = document.querySelector('#password-repeat')
+console.log(passwordRepeat)
+
+const passwordError = document.querySelector('#password + span.error');
+console.log(passwordError)
+
+const passwordRepeatError = document.querySelector('#password-repeat + span.error');
+console.log(passwordRepeatError)
